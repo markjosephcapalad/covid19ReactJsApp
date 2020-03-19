51976e7c139a7c11519e7971f5c4d77fee0fb62b
Mark Joseph Capalad <mark.joseph.capalad@codeandtheory.com>
2020-03-19 21:13:48 +0800

    Improvement-1
    1. Refactored inline css styles.
    2. Added formatting to numbers by thousands and millions.

---
+++ src/Util.js
@@ -0,0 +1,26 @@
+export function FormatNumber(num) {
+  let newStr;
+  console.log(num);
+  if (num !== undefined) {
+    newStr = String(num);
+
+    // thousands
+    if (newStr.length < 7 && newStr.length > 3) {
+      newStr =
+        newStr.substring(0, newStr.length - 3) +
+        "," +
+        newStr.substring(newStr.length - 3, newStr.length);
+    }
+    // millions
+    else if (newStr.length > 6) {
+      newStr =
+        newStr.substring(0, newStr.length - 6) +
+        "," +
+        newStr.substring(newStr.length - 6, newStr.length - 3) +
+        "," +
+        newStr.substring(newStr.length - 3, newStr.length);
+    }
+  }
+
+  return newStr;
+}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,10 @@
 import React from "react";
+import "./css/common.css";
 
 function Footer(props) {
-  const _style = {
-    height: "100px",
-    marginTop: "20px",
-    color: "lightcoral",
-    backgroundColor: "darkslategrey",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  };
-  return <div style={_style}>Copyright &copy; 2020. All rights reserved.</div>;
+  return (
+    <div className="footer">Copyright &copy; 2020. All Rights Reserved.</div>
+  );
 }
 
 export default Footer;
