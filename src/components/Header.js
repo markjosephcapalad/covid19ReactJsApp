c2a5e29dad4568968365562165a0699e0b0c568a
Mark Joseph Capalad <mark.joseph.capalad@codeandtheory.com>
2020-03-20 12:29:24 +0800

    Improvement: Header
    Added source of data in the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,12 @@ function Header(props) {
   return (
     <div className="header">
       <h1>COVID-19 Daily Update (Unofficial)</h1>
+      <span className="headerLink">
+        Source:
+        <a href="https://www.worldometers.info/coronavirus/" target="_blank">
+          Worldometer.Info/conornavirus
+        </a>
+      </span>
     </div>
   );
 }
